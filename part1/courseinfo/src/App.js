c61f16f80a326565720f5c6f1c3f017a3fcdff11
RoyallyCheese <johnjustinb@gmail.com>
2022-09-30 21:30:15 +0530

    commit #4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,9 +3,15 @@ import Content from "./content"
 import Header from "./header"
 import Total from "./total"
 import Hello from "./hello"
+import { useState } from 'react'
 
-const App = () => {
+const App = (props) => {
   
+  const [counter, setCounter ] = useState(0)
+
+
+  console.log('rendering...', counter)
+
   const name = 'John'
 
   const age = 10 
@@ -31,11 +37,14 @@ const App = () => {
 
 return (
   <div>
+    {counter}
     <Header course = {course.name}></Header>
     <Content parts = {course.parts} ></Content>
     <Total parts = {course.parts}> </Total>
     <Hello name={name} age={age} />
+    <button onClick={()=>setCounter(counter + 1)}>click</button>
   </div>
+  
 )
 
 }
